daddbc8756f4735f7a5c91a87bea0da7ade62565
Colk-tech <master@colk.dev>
2025-06-19 02:43:46 +0900

    add: make be able to run from `docker compose`

--- main.py
+++ main.py
@@ -3,25 +3,24 @@ import asyncio
 from analyze import main as analyze_main
 from register import main as register_main
 from retrieve import main as retrieve_main
+from reset import main as reset_main
 
 
 async def main() -> None:
+    print("Initializing database...")
+    await reset_main()
+    print("Done.")
+
     print("Retrieving cases from NDL...")
-
-    await retrieve_main()
-
+    retrieve_main()
     print("Done.")
 
     print("Registering documents into your database...")
-
     await register_main()
-
     print("Done.")
 
     print("Analyzing documents...")
-
     await analyze_main()
-
     print("Done.")
 
 
