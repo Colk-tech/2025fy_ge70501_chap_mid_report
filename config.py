be5a995acf975033137265ea5ca9c2839ca86c1d
Colk-tech <master@colk.dev>
2025-06-19 02:30:36 +0900

    add: add a new config `CASES_SAVE_DIR` and `CASES_SEARCH_WORD`

--- config.py
+++ config.py
@@ -12,5 +12,15 @@ class Config(BaseSettings):
         description="データベースの URL で、デフォルトは直下のディレクトリを用いる。",
     )
 
+    CASES_SAVE_DIR: str = Field(
+        default="./cases/",
+        description="ケースの保存先ディレクトリ。デフォルトは ./cases/ で、存在しない場合は自動的に作成される。",
+    )
+
+    CASES_SEARCH_WORD: str = Field(
+        default="図書館",
+        description="ケース検索時に使用する単語。デフォルトは「図書館」。",
+    )
+
 
 DEFAULT_CONFIG = Config()
